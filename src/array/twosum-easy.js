a75e47f3b65f360ad5328df2c1659712046dd8da
Lan Chu <hoanglan.ict@gmail.com>
2023-03-20 11:04:53 +0700

    twosum-easy v2

--- src/array/twosum-easy.js
+++ src/array/twosum-easy.js
@@ -1,23 +1,11 @@
-function binarySearch(n, arr = [], startIndex = 0, endIndex = arr.length - 1) {
-  if (startIndex > endIndex) return undefined;
-  const midIndex = Math.floor((endIndex + startIndex) / 2);
-  const midEl = arr[midIndex];
-  if (n === midEl.num) return midEl.index;
-  if (n < midEl.num) return binarySearch(n, arr, startIndex, midIndex - 1);
-  if (n > midEl.num) return binarySearch(n, arr, midIndex + 1, endIndex);
-}
-
 function twoSum(nums, target) {
-  const length = nums.length;
-  const sortedNums = nums
-    .map((num, i) => ({ num, index: i }))
-    .sort((a, b) => a.num - b.num);
-
-  for (let i = 0; i < length; i++) {
-    const currentEl = sortedNums[i];
-    const toFindEl = target - currentEl.num;
-    const foundIndex = binarySearch(toFindEl, sortedNums, i + 1, length - 1);
-    if (foundIndex !== undefined) return [currentEl.index, foundIndex];
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const currentEl = nums[i];
+    const toFindEl = target - currentEl;
+    const foundIndex = map.get(toFindEl);
+    if (foundIndex !== undefined) return [i, foundIndex];
+    map.set(currentEl, i);
   }
 }
 
